a84ce0f93b914248335a9140e8fadcdd6776ec58
root <szymon.krysztopolski@gmail.com>
2022-04-08 21:36:37 +0200

    test

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 
 int main(int argc, char **argv)
 {
-    srand(time(NULL));
     int size, tid;
 
     MPI_Status status;
@@ -13,10 +12,12 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &tid);
     
+    srand(tid);
+    
     int SZ=size; //liczba lozek = liczbie procesów (pozniej zmienic)
     int mode=(rand()%3)%2; //0 -> Sekundant, 1 -> Weteran
 
-    printf("proc: %d mode: %d",tid,size);
+    printf("proc: %d mode: %d\n",tid,mode);
 
     MPI_Finalize();
     return 0;
